fdefb1f2201630f5c09aa6d2b202e6ef4e145d5c
dstilesr <d.stiles.r@hotmail.com>
2021-02-13 14:32:17 -0500

    Remove redundant env variable

--- start-airflow.sh
+++ start-airflow.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export AIRFLOW__CORE__SQL_ALCHEMY_CONN=mysql+mysqlconnector://$MYSQL_USER:$MYSQL_PASSWORD@mysqldb:3306/$MYSQL_DATABASE
+
 # Initialize db
 if [ -f $(pwd)/airflow/dbcheck.txt ]
 then
